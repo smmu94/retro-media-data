e3f8957f959268a4f12b32a836d0731eed8d7d67
smmu94 <stephaniemmu@gmail.com>
2024-12-30 00:42:26 +0100

    Add translations

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const nextTranslate = require('next-translate-plugin');
-
-module.exports = nextTranslate({
+module.exports = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['en', 'es'],
-    defaultLocale: 'en',
-    localeDetection: false,
-  },
-});
+};
